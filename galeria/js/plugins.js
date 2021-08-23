0a47fd86051b9580e9bbebbc17c950561e4de993
camilovc11 <89413348+camilovc11@users.noreply.github.com>
2021-08-23 13:02:56 -0500

    Add files via upload

---
+++ galeria/js/plugins.js
@@ -0,0 +1,25 @@
+
+( function() {
+	var container, button, menu;
+
+	container = document.getElementById( 'navegacion' );
+	if ( ! container )
+		return;
+
+	button = container.getElementsByTagName( 'button' )[0];
+	if ( 'undefined' === typeof button )
+		return;
+
+	menu = container.getElementsByTagName( 'ul' )[0];
+
+	// Menu
+	if ( -1 === menu.className.indexOf( 'nav-menu' ) )
+		menu.className += ' nav-menu';
+
+	button.onclick = function() {
+		if ( -1 !== container.className.indexOf( 'toggled' ) )
+			container.className = container.className.replace( ' toggled', '' );
+		else
+			container.className += ' toggled';
+	};
+} )();
